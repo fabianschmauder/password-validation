d1bf0f5be5b8389f3eb4b4704c3c49267538f37e
fabianschmauder <fabian.schmauder@codecentric.de>
2020-05-18 09:19:31 +0200

    Validate password min length

---
+++ src/main/java/PasswordValidation.java
@@ -0,0 +1,5 @@
+public class PasswordValidation {
+  public static boolean validate(String password) {
+    return password.length() > 7;
+  }
+}
---
+++ src/test/java/PasswordValidationTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PasswordValidationTest {
+
+  @Test
+  @DisplayName("validate should return true when password is valid")
+  public void checkValidPassword(){
+    //GIVEN
+    String password = "s23f$f2a";
+
+    //WHEN
+    boolean validate = PasswordValidation.validate(password);
+
+    //THEN
+    assertTrue(validate);
+  }
+
+  @Test
+  @DisplayName("When Password length is smaller than 8 the password should be invalid")
+  public void checkPasswordMinLength(){
+    //GIVEN
+    String password = "s23f$f2";
+
+    //WHEN
+    boolean validate = PasswordValidation.validate(password);
+
+    //THEN
+    assertFalse(validate);
+  }
+
+}
